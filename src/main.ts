e5cd5f98e56f95c0a284386920cf99d3eac1faab
Mostefa Kamal Lala <mostefa.kamal-lala@cactusoft.ca>
2021-08-13 14:05:18 -0400

    Printing labels from pull request

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
+/* eslint-disable prettier/prettier */
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {Octokit} from '@octokit/rest'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const pull_number: string = core.getInput('pull_number')
+    core.debug(`Checking labels for pull request number ${pull_number}`)
+    const octokit = new Octokit()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const pull = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+      owner: 'octocat',
+      repo: 'hello-world',
+      pull_number: Number(pull_number)
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(`Labels ${pull.data.labels}`)
+
+
   } catch (error) {
     core.setFailed(error.message)
   }
