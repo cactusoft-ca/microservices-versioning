8bf37745b835a256e73b5324af13249ca0a3a6ff
Mostefa Kamal Lala <mostefa.kamal-lala@cactusoft.ca>
2021-08-17 10:34:13 -0400

    Changed code

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,40 @@
 /* eslint-disable prettier/prettier */
-import {setFailed, getInput, debug} from '@actions/core'
-import {context, getOctokit} from '@actions/github'
+import github from '@actions/github'
+import core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    const pull_number: string = getInput('pull_number')
-    const owner: string = getInput('owner')
-    const repo: string = getInput('pull_number')
-    const token: string = getInput('token')
+    const pull_number = Number(core.getInput('pull_number'))
+    const owner: string = core.getInput('owner')
+    const repo: string = core.getInput('pull_number')
+    const myToken: string = core.getInput('token')
 
-    debug(`Context repo owner: ${context.repo.owner}`)
-    debug(`Checking labels for pull request number ${pull_number}`)
+    core.debug(`Context repo owner: ${github.context.repo.owner}`)
+    core.debug(`Checking labels for pull request number ${pull_number}`)
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
 
-    const octokit = getOctokit(token)
+    const octokit = github.getOctokit(myToken)
 
+    // You can also pass in additional options as a second parameter to getOctokit
+    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
 
-    const pull = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+    const { data: pullRequest } = await octokit.rest.pulls.get({
       owner,
       repo,
-      pull_number: Number(pull_number)
+      pull_number,
+      mediaType: {
+        format: 'diff'
+      }
     })
 
-    debug(`Labels ${pull.data.labels}`)
+    core.debug(JSON.stringify(pullRequest))
+
+    // debug(`Labels ${pull.data.labels}`)
   } catch (error) {
-    setFailed(error.message)
+    core.setFailed(error.message)
   }
 }
 
